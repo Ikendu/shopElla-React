33ec98123ed4ea7cc22b3b6977a84c9c0e6995f4
Chibunud <ndubest56@gmail.com>
2023-10-22 18:57:38 +0100

    main app with global state

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+import Products from './features/CBody/Products'
+import Card from './features/DCart/Card'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Card />
+        <Products />
+      </div>
+    </>
+  )
+}
+
+export default App
