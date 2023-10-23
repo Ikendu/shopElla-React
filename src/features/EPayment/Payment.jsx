dc5f035b886a9100d94c25f9f9808185006f62b5
Chibunud <ndubest56@gmail.com>
2023-10-23 20:38:55 +0100

    payment model added

--- src/features/EPayment/Payment.jsx
+++ src/features/EPayment/Payment.jsx
@@ -8,38 +8,45 @@ const Payment = () => {
   return (
     <div className='modal-container'>
       <div className='modal'>
-        <h2>Your purchase details</h2>
+        <button onClick={() => dispatch(closeModel())}>Back to Shopping</button>
+        <h2>Your Purchase Details</h2>
         <div className='summary'>
-          <div>
-            <p>name</p>
-            <p>price</p>
-            <p>count</p>
-            <p>amount</p>
-          </div>
-          {sales.map((item) => (
-            <div className='each-details' key={item.id}>
-              <span className='name-item'>{item.name}</span>
-              <span className='price-item'> {item.price}</span>
-              <span className='count-item'>{item.count}</span>
-              <span> {item.price * item.count}</span>
-            </div>
-          ))}
+          <table className='table'>
+            <caption>Preview Your Purchase Details</caption>
+            <tr>
+              <th>Modal</th>
+              <th>Price</th>
+              <th>Qty</th>
+              <th>Amount</th>
+            </tr>
+            {sales.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td> {item.price}</td>
+                <td>{item.count}</td>
+                <td> {item.price * item.count}</td>
+              </tr>
+            ))}
+          </table>
+          <div className='heading-item'></div>
         </div>
         <h3 className='price'>Total Amount: {total}</h3>
 
         <h2>Payment Details</h2>
         <div className='account'>
           <p>You can complete your payment using Mobile Transfer, POS or Bank deposite.</p>
-          <p>Account Details</p>
           <h3>Name: Idoko Gift Onyinyechi</h3>
           <h3>Account No: 3121077326</h3>
           <h3>Bank name: First Bank</h3>
         </div>
 
-        <p>After payment is completed, send prove of payment to +2348023613691 through Whatsap</p>
-        <p>Your can also call the above number or send us email through ellagift25@gmail.com</p>
+        <p>
+          After payment send prove of payment to 08023613691 through Whatsap for comfirmation and
+          subsequent packaging of your items
+        </p>
+        <p>email: ellagift25@gmail.com</p>
         <h3>Thank you for your patronage we look forward to seeing you again</h3>
-        <button onClick={() => dispatch(closeModel())}>Back to Page</button>
+        <button onClick={() => dispatch(closeModel())}>Back to Shopping</button>
       </div>
     </div>
   )
